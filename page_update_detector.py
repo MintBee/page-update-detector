b50b19747bd4170feb17dc8673b14df38adebb82
박동윤 <ehddbs7458@gmail.com>
2023-02-26 23:19:05 +0900

    Remove unnecessary comment.

--- page_update_detector.py
+++ page_update_detector.py
@@ -7,9 +7,6 @@ class PageUpdateDetector:
         self.last_page = last_page
         self.keywords = keywords
 
-    # Compare current_page html and last_page html. If there is a difference, return True.
-    # Otherwise, return False.
-    # Also, return the list of keywords that is included in updated part of current keyword.
     def detect(self):
         assert self.current_page is not None and self.last_page is not None
         if self.current_page == self.last_page:
